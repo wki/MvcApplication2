2bd9c1149374e6a283f32cc016ea5752b520bdef
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-05-02 21:52:16 +0200

    added simple MessageQ based on RabbitMQ

---
+++ MessageQ/MessageQ.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQ
+{
+    /* usage (producer)
+     * 
+     * var queue = MessageQ->new;
+     * queue->Publish('render', renderOptions);
+     * 
+     */
+    public class MessageQ
+    {
+        public MessageQ()
+        {
+           Channel().ExchangeDeclare("cards", "direct");
+        }
+
+        private IModel Channel()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            return connection.CreateModel();
+        }
+
+        public void Publish(string key, object data)
+        {
+            var channel = Channel();
+            var body = Encoding.UTF8.GetBytes(data.ToString());
+            var basicProperties = channel.CreateBasicProperties();
+            basicProperties.Headers = new Dictionary<string, object>();
+            basicProperties.DeliveryMode = 1;
+            channel.BasicPublish("cards", key, basicProperties, body);
+        }
+    }
+}
