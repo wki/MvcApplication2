c4733eac088f8c40c363ad52a227395cb8cd7ca7
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-05-16 21:31:56 +0200

    changed EventBus and Tests to Castle Windsor. With a trick contravariant submit-events are resolved

--- EventBus.Tests/EventBusTest.cs
+++ EventBus.Tests/EventBusTest.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using Microsoft.Practices.Unity;
+﻿using Castle.Windsor;
+using System;
+// using Microsoft.Practices.Unity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
 using EventBus;
 using System.Reflection;
+using Castle.MicroKernel.Registration;
 
 namespace EventBus.Tests
 {
-    class GoodThingHappened    : IEvent {}
-    class BadThingHappened     : IEvent { }
-    class AnotherThingHappened : IEvent { }
+    public class GoodThingHappened    : IEvent {}
+    public class BadThingHappened     : IEvent { }
+    public class AnotherThingHappened : IEvent { }
 
-    class ServiceBase
+    public class ServiceBase
     {
         public int nrEventsHandled;
 
@@ -22,7 +24,11 @@ namespace EventBus.Tests
         }
     }
 
-    class AService : ServiceBase, ISubscribe<GoodThingHappened>
+    interface IAService {}
+    interface IBService {}
+    interface ICService {}
+
+    public class AService : ServiceBase, IAService, ISubscribe<GoodThingHappened>
     {
         public AService()
         {
@@ -34,7 +40,7 @@ namespace EventBus.Tests
         }
     }
 
-    class BService : ServiceBase, ISubscribe<BadThingHappened>
+    public class BService : ServiceBase, IBService, ISubscribe<BadThingHappened>
     {
         public BService()
         {
@@ -46,7 +52,7 @@ namespace EventBus.Tests
         }
     }
 
-    class CService : ServiceBase, ISubscribe<IEvent>
+    public class CService : ServiceBase, ICService, ISubscribe<IEvent>
     {
         public CService()
         {
@@ -62,20 +68,35 @@ namespace EventBus.Tests
     [TestClass]
     public class EventBusTest
     {
-        private IUnityContainer container { get; set; }
+        // private IUnityContainer container { get; set; }
+        private IWindsorContainer container { get; set; } 
         
         private void printRegistrations()
         {
-            foreach (ContainerRegistration item in container.Registrations)
+            // Unity only:
+            //foreach (ContainerRegistration item in container.Registrations)
+            //{
+            //    Console.WriteLine(item.GetMappingAsString());
+            //}
+
+            // Windsor only:
+            Console.WriteLine("Registrations:");
+            foreach (var handler in container.Kernel.GetAssignableHandlers(typeof(object)))
             {
-                Console.WriteLine(item.GetMappingAsString());
+                foreach (var service_name in handler.ComponentModel.Services)
+                {
+                    Console.WriteLine("Service: {0} Name: {1}, Implemented By {2}",
+                        service_name,
+                        handler.ComponentModel.Name,
+                        handler.ComponentModel.Implementation);
+                }
             }
         }
 
         [TestInitialize]
         public void InitializeContainer()
         {
-            container = new UnityContainer();
+            container = new WindsorContainer();
         }
 
         // HELP! do not know how to check "uninitialized".
@@ -113,7 +134,10 @@ namespace EventBus.Tests
         {
             // arrange
             var aService = new AService();
-            container.RegisterInstance<ISubscribe<GoodThingHappened>>("aService", aService);
+            // container.RegisterInstance<ISubscribe<GoodThingHappened>>("aService", aService);
+            container.Register(
+                Component.For<ISubscribe<GoodThingHappened>>().Instance(aService)
+            );
             printRegistrations();
             var bus = new EventBus(container);
 
@@ -130,8 +154,13 @@ namespace EventBus.Tests
             // arrange
             var aService = new AService();
             var bService = new BService();
-            container.RegisterInstance<ISubscribe<GoodThingHappened>>("aService", aService);
-            container.RegisterInstance<ISubscribe<BadThingHappened>>("bService", bService);
+            //container.RegisterInstance<ISubscribe<GoodThingHappened>>("aService", aService);
+            //container.RegisterInstance<ISubscribe<BadThingHappened>>("bService", bService);
+            container.Register(
+                Component.For<ISubscribe<GoodThingHappened>>().Instance(aService),
+                Component.For<ISubscribe<BadThingHappened>>().Instance(bService)
+            );
+            
             printRegistrations();
             var bus = new EventBus(container);
 
@@ -151,8 +180,12 @@ namespace EventBus.Tests
             // arrange
             var a1Service = new AService();
             var a2Service = new AService();
-            container.RegisterInstance<ISubscribe<GoodThingHappened>>("a1Service", a1Service);
-            container.RegisterInstance<ISubscribe<GoodThingHappened>>("a2Service", a2Service);
+            //container.RegisterInstance<ISubscribe<GoodThingHappened>>("a1Service", a1Service);
+            //container.RegisterInstance<ISubscribe<GoodThingHappened>>("a2Service", a2Service);
+            container.Register(
+                Component.For<ISubscribe<GoodThingHappened>>().Named("a1").Instance(a1Service),
+                Component.For<ISubscribe<GoodThingHappened>>().Named("a2").Instance(a2Service)
+            );
             printRegistrations();
             var bus = new EventBus(container);
 
@@ -170,34 +203,55 @@ namespace EventBus.Tests
         public void EventBus_Publish_Generic_Subscribed_Event()
         {
             // arrange
-            var aService = new AService();
-            var cService = new CService();
-            container.RegisterInstance<ISubscribe<GoodThingHappened>>("aService", aService);
-            container.RegisterInstance<ISubscribe<IEvent>>("cService", cService);
+            //var aService = new AService();
+            //var cService = new CService();
+            //container.RegisterInstance<ISubscribe<GoodThingHappened>>("aService", aService);
+            //container.RegisterInstance<ISubscribe<IEvent>>("cService", cService);
+            container.Register(
+                //Component.For<ISubscribe<GoodThingHappened>>().Instance(aService),
+                //Component.For<ISubscribe<IEvent>>().Instance(cService),
+                Classes.FromThisAssembly()
+                    // .FromAssemblyContaining<EventBusTest>()
+                    .BasedOn<ServiceBase>()
+                    .WithService.AllInterfaces()
+                    // .WithService.Base()
+                    // .WithService.Self()
+                    .Configure(component => component.Named(component.Implementation.FullName + "XYZ"))
+                    .LifestyleSingleton()
+                    // .WithService.AllInterfaces()
+                    //.LifeStyle.Singleton
+            );
+
+            AService aService = (AService) container.Resolve<IAService>();
+            CService cService = (CService) container.Resolve<ICService>();
             
             // var interfaces = new List<Type>(instanceType.GetInterfaces());
 
-            var is_assignable = typeof(ISubscribe<IEvent>)
-                .IsAssignableFrom(typeof(ISubscribe<BadThingHappened>));
-            Console.WriteLine("ISubscribe<IEvent> isAssignableFrom ISubscribe<BadThingHappened>: " + is_assignable);
-            
-            is_assignable = typeof(IEvent).GetType()
-                .IsAssignableFrom(typeof(BadThingHappened).GetType());
-            Console.WriteLine("IEvent isAssignableFrom BadThingHappened: " + is_assignable);
-            
-            is_assignable = typeof(BadThingHappened).GetType()
-                .IsAssignableFrom(typeof(IEvent).GetType());
-            Console.WriteLine("BadThingHappened isAssignableFrom IEvent: " + is_assignable);
+            //var is_assignable = typeof(ISubscribe<IEvent>)
+            //    .IsAssignableFrom(typeof(ISubscribe<BadThingHappened>));
+            //Console.WriteLine("ISubscribe<IEvent> isAssignableFrom ISubscribe<BadThingHappened>: " + is_assignable);
 
-            Type instanceType = typeof(BadThingHappened); // typeof(ISubscribe<IEvent>);
-            Console.WriteLine("Type: " + instanceType);
-            instanceType
-                // .FindInterfaces(new TypeFilter())
-                .GetInterfaces()
-                // .Where(x => x.IsGenericType)
-                .ToList()
-                .ForEach(x => Console.WriteLine("Interface Type: " + x));
-                // .ForEach(x => container.RegisterInstance(x, cService, x.Name));
+            //is_assignable = typeof(ISubscribe<BadThingHappened>)
+            //    .IsAssignableFrom(typeof(ISubscribe<IEvent>));
+            //Console.WriteLine("ISubscribe<BadThingHappened> isAssignableFrom ISubscribe<IEvent>: " + is_assignable);
+            
+            //is_assignable = typeof(IEvent).GetType()
+            //    .IsAssignableFrom(typeof(BadThingHappened).GetType());
+            //Console.WriteLine("IEvent isAssignableFrom BadThingHappened: " + is_assignable);
+            
+            //is_assignable = typeof(BadThingHappened).GetType()
+            //    .IsAssignableFrom(typeof(IEvent).GetType());
+            //Console.WriteLine("BadThingHappened isAssignableFrom IEvent: " + is_assignable);
+
+            //Type instanceType = typeof(BadThingHappened); // typeof(ISubscribe<IEvent>);
+            //Console.WriteLine("Type: " + instanceType);
+            //instanceType
+            //    // .FindInterfaces(new TypeFilter())
+            //    .GetInterfaces()
+            //    // .Where(x => x.IsGenericType)
+            //    .ToList()
+            //    .ForEach(x => Console.WriteLine("Interface Type: " + x));
+            //    // .ForEach(x => container.RegisterInstance(x, cService, x.Name));
 
             printRegistrations();
             var bus = new EventBus(container);
--- PubSub/EventBus.cs
+++ PubSub/EventBus.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Practices.Unity;
+﻿using Castle.Windsor;
+// using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,12 @@ namespace EventBus
 {
     public class EventBus : IEventBus
     {
-        private readonly IUnityContainer container;
+        //private readonly IUnityContainer container;
+        private readonly IWindsorContainer container;
 
         public static IEventBus Current { get; private set; }
 
-        public EventBus(IUnityContainer container)
+        public EventBus(IWindsorContainer container)
         {
             // maybe we should die if Current is already set.
             Current = this;
@@ -23,39 +25,46 @@ namespace EventBus
 
         public void Publish<T>(T @event) where T : class, IEvent
         {
+            Console.WriteLine("");
             _publish<T>(@event, false);
         }
 
-        private void _publish<T>(T @event, bool recursive) where T : class, IEvent
+        public void _publish<T>(T @event, bool recursive) where T : class, IEvent
         {
             Console.WriteLine(
-                String.Format("Publishing: {0}", @event.GetType())
+                String.Format("Publishing: {0} Type: {1}", @event.GetType(), typeof(T))
             );
             var eventHandlers = container.ResolveAll<ISubscribe<T>>();
             foreach (var eventHandler in eventHandlers)
             {
+                Console.WriteLine("Handler: " + eventHandler);
                 eventHandler.Handle(@event);
             }
 
+            
             // IDEA: call Publish recursively for Base-Class and all Interfaces
             // Remember no of called event handlers and don't call them repeatedly
             // if no handlers are found.
 
-            @event
-                .GetType()
-                .GetInterfaces()
-                .ToList()
-                .ForEach(x => Console.WriteLine("Would Call: " + x));
-
+            //@event
+            //    .GetType()
+            //    .GetInterfaces()
+            //    .ToList()
+            //    .ForEach(x => Console.WriteLine("Would Call: " + x));
+            
+            
             if (!recursive)
             {
                 foreach (var t in @event.GetType().GetInterfaces())
                 {
-                    //Console.WriteLine("Must call: " + t);
-                    MethodInfo publishMethod = this.GetType().GetMethod("publish").MakeGenericMethod(t);
+                    // Console.WriteLine("Must call: " + this.GetType().GetMethod("_publish"));
+                    MethodInfo publishMethod = this.GetType().GetMethod("_publish").MakeGenericMethod(t);
+
+                    Console.WriteLine("Method: " + publishMethod);
                     publishMethod.Invoke(this, new object[] { @event, true });
                 }
             }
+            
 
             /*
             
