d7f2afb409cf5401fa55b42ca1b65e78a19ab65c
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-06-01 19:34:30 +0200

    added a boolean return value indicating successful dispatching of events

--- PubSub/Hub.cs
+++ PubSub/Hub.cs
@@ -9,6 +9,15 @@ using System.Threading.Tasks;
 
 namespace EventBus
 {
+    /*
+     * Idea for expansion: split into several classes:
+     *
+     * Hub -- Publish() method, delegates to dispatcher.Dispatch()
+     * IDispatcher -- dispatcher interface forces Dispatch<T>(T @event) method
+     * WindsorDispatcher -- Dispatch() method resolves services using Windsor
+     * InterfacesDispatchDecorator -- Dispatch() implemented interfaces also
+     *
+     */
     public class Hub : IHub
     {
         public static ILog Log = LogManager.GetCurrentClassLogger();
@@ -27,29 +36,32 @@ namespace EventBus
         {
             Log.Debug(m => m("Publish: {0} Type: {1}", @event.GetType(), typeof(T)));
             
-            DispatchEventClass<T>(@event);
-            DispatchParentInterfaces(@event);
+            Dispatch<T>(@event);
+            DispatchInterfaces(@event);
         }
 
         // must be public to allow reflection to find it
-        public void DispatchEventClass<T>(T @event) where T : class, IEvent
+        // return value may be of interest to caching logic.
+        public bool Dispatch<T>(T @event) where T : class, IEvent
         {
-            T dummy = @event;
-            var eventHandlers = container.ResolveAll<ISubscribe<T>>();
-            foreach (var eventHandler in eventHandlers)
+            bool event_handled = false;
+            foreach (var eventHandler in container.ResolveAll<ISubscribe<T>>())
             {
                 Log.Debug(m => m("Handler: {0}", eventHandler));
                 eventHandler.Handle(@event);
+                event_handled = true;
             }
+
+            return event_handled;
         }
 
-        public void DispatchParentInterfaces(IEvent @event)
+        public void DispatchInterfaces(IEvent @event)
         {
             // Hint: this Query could be cached.
             foreach (var t in @event.GetType().GetInterfaces().Where(t => typeof(IEvent).IsAssignableFrom(t)))
             {
                 Log.Debug(m => m("Trying to Dispatch Type: {0}", t.FullName));
-                MethodInfo publishMethod = this.GetType().GetMethod("DispatchEventClass").MakeGenericMethod(t);
+                MethodInfo publishMethod = this.GetType().GetMethod("Dispatch").MakeGenericMethod(t);
 
                 publishMethod.Invoke(this, new object[] { @event } );
             }
