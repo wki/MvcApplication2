02dd7f2a35d01e2b06af426815eb00fcde2984bf
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-06-29 20:01:33 +0200

    added owin references and startup file. moved all startup relevant classes into App_Start folder

---
+++ Web/App_Start/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin;
+using System.Threading.Tasks;
+using Owin;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[assembly: OwinStartupAttribute(typeof(Web.Startup))]
+namespace Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // ConfigureAuth(app); // copied from AuthApp -- activate when ready.
+            app.Use<WkComponent>(new string[] { "A Special Header for you" });
+        }
+
+    }
+
+    /// <summary>
+    /// a stupid simple middleware adding a response header.
+    /// </summary>
+    public class WkComponent : OwinMiddleware
+    {
+        private string WkHeader { get; set; }
+
+        public WkComponent(OwinMiddleware next, string header)
+            : base(next)
+        {
+            WkHeader = header;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            context.Response.Headers["X-Modified-WK"] = WkHeader;
+
+            return Next.Invoke(context);
+        }
+    }
+}
