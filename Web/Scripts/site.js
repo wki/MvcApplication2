5b94c3e8c78737e534728cfd0ec0274af7153025
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-07-03 21:16:28 +0200

    playing around with angular

--- Web/Scripts/site.js
+++ Web/Scripts/site.js
@@ -3,45 +3,57 @@
  *
 */
 
-var app = angular.module("WkiApp", ["ngResource"]);
+(function () {
+    var app = angular.module("WkiApp", []);
 
-/////////////////// Simple Binding Demo
+    /////////////////// Simple Binding Demo
 
-app.service("greeter", function () {
-    this.name = "foo"
-    this.greeting = function () {
-        return this.name
-            ? ("Hello, " + this.name + "!")
-            : "";
-    };
-});
+    app.service("greeter", function () {
+        this.name = "foo"
+        this.greeting = function () {
+            return this.name
+                ? ("Hello, " + this.name + "!")
+                : "";
+        };
+    });
 
-// WORKS (when *NOT* minified):
-//app.controller("StartupController", function ($scope, greeter) {
-//    $scope.greeter = greeter;
-//});
+    // WORKS (when *NOT* minified):
+    //app.controller("StartupController", function ($scope, greeter) {
+    //    $scope.greeter = greeter;
+    //});
 
-// ALSO WORKS (even when minified):
-app.controller("StartupController", ["$scope", "greeter", function ($scope, greeter) {
-    $scope.greeter = greeter;
-}]);
+    // ALSO WORKS (even when minified):
+    app.controller("StartupController", ["$scope", "greeter", function ($scope, greeter) {
+        $scope.greeter = greeter;
+    }]);
 
 
-/////////////////// Info Loader
+    /////////////////// Info Loader
 
-app.factory("infoResource", function ($resource) {
-    return $resource("/api/Info/:id", { id: "@id" });
-});
+    app.controller(
+        "InfoController",
+        ["$scope", "$http", "$interval", function ($scope, $http, $interval)
+    {
+        $scope.id = 43;
+        $scope.data = "hello";
+        $scope.counter = 10;
 
-app.service("info", function (infoResource) {
-    this.id = 5;
-    this.data = function () {
-        console.log(infoResource);
-        // return infoResource.get({ id: this.id });
-        return "xxx" + this.id;
-    };
-});
+        $interval(decrementCounter, 500, $scope.counter);
 
-app.controller("InfoController", ["$scope", "info", function ($scope, info) {
-    $scope.info = info;
-}]);
+        function decrementCounter() {
+            if (--$scope.counter <= 0) {
+                $scope.counter = 10;
+            }
+        }
+
+        function onDataReceived(response) {
+            $scope.data = response.data;
+        }
+
+        $scope.update = function () {
+            $http.get("/api/Info/" + $scope.id)
+                .then(onDataReceived);
+        };
+    }]);
+
+}());
