14ee6912f1f782bdd1b75c3296173b9189db1f8c
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-07-10 20:57:10 +0200

    updated DDDSkeleton classes based on ideas from Julie Lerman's Video

--- DDDSkeleton/Domain/Entity.cs
+++ DDDSkeleton/Domain/Entity.cs
@@ -7,23 +7,17 @@ using System.Threading.Tasks;
 
 namespace DDDSkeleton.Domain
 {
-    public class Entity<IdType> : DomainObject, IPublish
+    public class Entity<IdType> : IEquatable<Entity<IdType>>, DomainObject, IPublish
     {
         public IdType Id { get; private set; }
 
-        public Entity()
+        // needed  ny EF
+        protected Entity()
         { }
 
         public Entity(IdType id)
         {
             Id = id;
-        }
-
-        public override bool Equals(Object entity)
-        {
-            return entity != null
-                && entity is Entity<IdType>
-                && this == (Entity<IdType>)entity;
         }
 
         public override int GetHashCode()
@@ -49,6 +43,13 @@ namespace DDDSkeleton.Domain
         public static bool operator !=(Entity<IdType> entity1, Entity<IdType> entity2)
         {
             return !(entity1 == entity2);
+        }
+
+        public override bool Equals(Entity<IdType> other)
+        {
+            return other != null
+                && other is Entity<IdType>
+                && this == (Entity<IdType>)other;
         }
     }
 }
