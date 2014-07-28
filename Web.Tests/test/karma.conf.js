4371e23c156d123bc50783be4a83d4e0123d16f0
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-07-28 21:57:24 +0200

    switched to jasmine test framework

--- Web.Tests/test/karma.conf.js
+++ Web.Tests/test/karma.conf.js
@@ -1,8 +1,9 @@
 ﻿module.exports = function (config) {
     config.set({
         basePath: '../',
-        frameworks: ['mocha'],
-        reporters: ['progress', 'dots'],
+        //frameworks: ['mocha'],
+        frameworks: ['jasmine'],
+        reporters: ['progress'],
         browsers: ['Chrome'],
         autoWatch: false,
         port: 9876,
@@ -16,14 +17,15 @@
           // 3rd Party Code
           '../Web/Scripts/angular.js',
           '../Web/Scripts/angular-route.js',
+          '../Web/Scripts/angular-mocks.js',
 
           // App-specific Code
           '../Web/Scripts/site.js',
 
-          // Test-Specific Code
-          'test/mocha.conf.js',
-          'node_modules/chai/chai.js',
-          'test/lib/chai-sugar.js',
+          // Test-Specific Code (only for mocha/chai)
+          // 'test/mocha.conf.js',
+          //  'node_modules/chai/chai.js',
+          // 'test/lib/chai-sugar.js',
 
           // unit tests
           'test/unit/**/*.js'
--- Web.Tests/test/unit/minimal_test.js
+++ Web.Tests/test/unit/minimal_test.js
@@ -1,6 +1,42 @@
 ﻿describe("Minimal Test", function () {
     it("should pass", function () {
         var x = [];
-        expect(x).not.to.be.a('null');
+        // expect(x).not.to.be.a('null');
+        expect(x).not.toBe(null);
+        expect(x).toBeTruthy();
+    });
+
+    it("should fail", function () {
+        var y = false;
+
+        expect(y).toBe(false);
+    });
+});
+
+describe("Module", function () {
+    var module;
+    beforeEach(function () {
+        module = angular.module("WkiApp");
+    });
+
+    it("should be registered", function () {
+        expect(module).toBeDefined();
+    });
+
+    it("should not resolve an unknown Controller", function () {
+        var controller = module.controller("DummyNotExistingController");
+        expect(controller).not.toBeDefined();
+    });
+});
+
+describe("Order Controller", function () {
+    var module;
+    beforeEach(function () {
+        module = angular.module("WkiApp");
+    });
+
+    it("should be resolvable", function () {
+        var controller = module.controller("OrderController");
+        expect(controller).toBeDefined();
     });
 });
